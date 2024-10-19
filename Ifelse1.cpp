f7d81b04ad9e96ce266ba80b1a108c4c9c84f91d
Muhammad falid faqqy <88636867+Kancil173@users.noreply.github.com>
2024-10-19 13:03:40 +0700

    Add files via upload

---
+++ Ifelse1.cpp
@@ -0,0 +1,13 @@
+// MUHAMMAD FALID FAQQY
+#include <iostream>
+using namespace std;
+int main() {
+    int nilai;
+       cout<<"Masukan Nilai Anda : ";
+    cin>>nilai;
+      if(nilai>75)
+          cout<<"\nLULUS";
+      else
+         cout<<"\nTIDAK LULUS";
+    return 0;
+}
