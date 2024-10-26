fd537af792e458c260f9278cc67a8d5059aec0ac
Muhammad falid faqqy <88636867+Kancil173@users.noreply.github.com>
2024-10-26 13:37:24 +0700

    Add files via upload

---
+++ forwhilefalid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+  int main() {
+    cout << "Bilangan" << endl;
+    cout << "Ganjil : ";
+    
+    for (int i = 1; i <= 35; i += 2) {  
+        cout << i << " ";
+    }
+    cout << endl << "Genap  : ";
+    
+    int j =2;
+    while (j <= 34) {
+        cout << j << " ";
+        j += 2;
+    }
+    cout<<"---------------------------";
+
+    
+    return 0;
+}
